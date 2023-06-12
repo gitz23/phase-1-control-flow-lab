1fcae75108167e6ed6f16a5d07ccf2a1d48175a4
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-13 00:32:14 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result 
+  if (someValue <= 400){
+    return 'This one is on me!';
+  }else if (someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else(someValue > 2500)
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === "NYC"? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipService){
   // Write your code here!
+  switch(tipService){
+    case 'generous':
+     return 'Thank you so much.'
+     
+     case 'not as generous':
+      return 'Thank you.'
+    
+    case 'thanks for everything':
+    return 'Bye.'
+  }
 }
